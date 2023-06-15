490c5b0d6ef383463dd18c32955aac4aaa2899f7
Margarita Rastriga <rasm@1Ci.com>
2023-06-16 01:50:38 +0300

    Класс linked_list и тесты к ним

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,52 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.__data = data
+        self.next_node = None
+
+    @property
+    def data(self):
+
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        self.__data = value
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node_to_add = Node(data)
 
+        if self.head is not None:
+            node_to_add.next_node = self.head
+        self.head = node_to_add
+
+        if self.tail is None:
+            self.tail = node_to_add
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node_to_add = Node(data)
+
+        if self.tail is not None:
+            self.tail.next_node = node_to_add
+        self.tail = node_to_add
+
+        if self.head is None:
+            self.head = node_to_add
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +60,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
